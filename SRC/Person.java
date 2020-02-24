9a98b5a441e881969e5753d2a6c0728dcb65f63d
priyanshu <priyanshu.pathak_iot18@gla.ac.in>
2020-02-24 22:28:12 +0530

    getter setters and toString  are created for person class
    Signed-off-by: priyanshu <priyanshu.pathak_iot18@gla.ac.in>

--- SRC/Person.java
+++ SRC/Person.java
@@ -10,7 +10,53 @@
          this.contactno = contactno;
          this.email = email;
      }
+     public String getFirstname() {
+         return firstname;
+     }
 
+     public void setFirstname(String firstname) {
+         this.firstname = firstname;
+     }
+
+     public String getLastname() {
+         return lastname;
+     }
+
+     public void setLastname(String lastname) {
+         this.lastname = lastname;
+     }
+
+     public String[] getContactno() {
+         return contactno;
+     }
+
+     public void setContactno(String[] contactno) {
+         this.contactno = contactno;
+     }
+
+     public String getEmail() {
+         return email;
+     }
+
+     public void setEmail(String email) {
+         this.email = email;
+     }
+
+     public String toString() {
+         final StringBuffer sb = new StringBuffer("-------- * -------- * -------- * --------\n");
+         sb.append("firstName=").append(firstname).append('\n');
+         sb.append("lastName=").append(lastname).append('\n');
+         sb.append("contactno=").append(contactno);
+
+         for (int i = 0; i < contactno.length; i++) {
+             sb.append((contactno[i] != null && i != contactno.length ? contactno[i] : ""));
+
+         }
+         sb.append("\nemail='").append(email).append('\n');
+         sb.append("-------- * -------- * -------- * --------");
+         return sb.toString();
+     }
+ }
 
 
 }
