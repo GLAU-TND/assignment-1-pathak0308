51196db33cddfa44f3fdcb67462dc1d32a8202b5
priyanshu <priyanshu.pathak_iot18@gla.ac.in>
2020-02-24 22:26:26 +0530

    constructor created
    Signed-off-by: priyanshu <priyanshu.pathak_iot18@gla.ac.in>

---
+++ SRC/Person.java
@@ -0,0 +1,16 @@
+ public class Person {
+        private String firstname = "";
+        private String lastname = "";
+        private String[] contactno;
+        private String email = "";
+
+     public Person(String firstname, String lastname, String[] contactno, String email) {
+         this.firstname = firstname;
+         this.lastname = lastname;
+         this.contactno = contactno;
+         this.email = email;
+     }
+
+
+
+}
