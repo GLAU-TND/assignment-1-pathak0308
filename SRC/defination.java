673aadb03b0fc11d340444ad9c89a8b1759cff60
priyanshu <priyanshu.pathak_iot18@gla.ac.in>
2020-02-24 19:54:53 +0530

    defination class is created which implements ADT

---
+++ SRC/defination.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class defination<Person> implements ADT<Person> {
+
+    @Override
+    public boolean add(Person details) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int index) {
+        return false;
+    }
+
+    @Override
+    public void search(String person) {
+
+    }
+
+    @Override
+    public void viewAllDetails() {
+
+    }
+}
